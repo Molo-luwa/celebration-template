81ded003477f214c8063985f9d22dffab61b2fb7
mololuwa <tomisola2008@gmail.com>
2026-04-28 17:22:19 +0100

    plug and play file

---
+++ data.js
@@ -0,0 +1,64 @@
+//replace each value with your own information and images. 
+//texts can be as long or as short as you like.
+//You can also add more sections if you like. 
+//Just make sure to update the HTML accordingly.
+
+
+emojiEffects: false //set to true to enable falling emoji effects 
+// (can be turned on/off without affecting the rest of the page)
+
+const data = {
+  // BASIC INFO
+  wish: "CONGRATULATIONS",
+  name: "Scott Chisom",
+  relation: "My Best Friend • My Sister • My Pride",
+  password: "chisom",
+
+  // SECTION 1
+  header_one: "To the Woman of the Moment",
+  paragraph_one: `
+    Chisom, seeing you in your convocation gown isn't just a celebration of a degree.
+    It’s a celebration of your soul. You didn't just survive; you bloomed.
+  `,
+  highlight: `
+    "I love you so much, more than words could ever weave together in a lifetime."
+  `,
+  paragraph_two: `
+    Every late-night call, every shared laugh, and every tear has led to this moment.
+    You are a masterpiece in progress.
+  `,
+
+  // SECTION 2
+  header_two: "A Prayer & A Promise",
+  paragraph_three: `
+    My dearest Chisom, as you step out of those campus gates,
+    <strong>I pray that God grants all your heart's desires.</strong>
+    I pray that the road ahead is filled with favor.
+  `,
+  paragraph_four: `
+    <strong>We remain friends forever.</strong>
+    No distance, no time will break what we have.
+  `,
+
+  // SECTION 3
+  header_three: "The Love I Carry for You",
+  paragraph_five: `
+    If I wrote a thousand pages, I still couldn't finish telling you how much you mean to me. You are my safe haven. Thank you for being the kind of friend who listens with her heart. Thank you for your strength, which inspires me daily.
+  `,
+
+  // FINAL
+  final_message: `
+    I love you so much, Chisom. <br>
+    Today, tomorrow, and for all eternity.
+  `,
+    footer:
+  `Designed for Chisom's Big Day | Class of 2025`,
+
+  // IMAGES
+  images: {
+    pic1: "images/pic1.jpg",
+    pic2: "images/pic2.jpg",
+    pic3: "images/pic3.jpg"
+  }
+};
+
